a6bf9cee8efae635c56befbed85b2ec6852615d3
Fern <fmenezes098@gmail.com>
2025-01-22 12:21:48 -0300

    :sparkles: Criação da Classe Usuário

---
+++ src/usuarios/usuario.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './entities/usuario.entity';
+import { UsuarioService } from './services/usuario.service';
+import { UsuarioController } from './controller/usuario.controller';
+
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Usuario])], 
+  providers: [UsuarioService],
+  controllers: [UsuarioController],
+  exports: [UsuarioService],
+})
+export class UsuarioModule {}
